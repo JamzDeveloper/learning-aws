f577f3ae0d5182ced1398f097d698a9982ea5f28
jamzdeveloper <jamzdeveloper@gmail.com>
2022-04-02 19:53:36 -0500

    update

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express()
 
 app.get("/",(req,res)=>res.send("<h1>hello word from nodejs with htlm</h1>>"))
-app.listen(3000);
-console.log("server on port 3000");
+app.listen(8080);
+console.log("server on port 8080");
